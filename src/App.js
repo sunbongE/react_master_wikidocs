b25d9d0e40a40bfd6fb359aeb95f4933e5afe34b
Taeho <qkrxogh7@gmail.com>
2025-07-10 16:55:43 +0900

    중복 허용x, 우승자 표출

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import { useState } from "react"
 
+function calculateWinner(squares){
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if(squares[a] && squares[b] === squares[a] && squares[a] === squares[c]){
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 function Square({value, onSquareClick}){
 
   return (
@@ -14,19 +35,30 @@ export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
 
   function handleClick(i){
-    console.log('squares : ',squares)
+    if(squares[i] || calculateWinner(squares)){
+      return;
+    }
+
     const nextSquares = squares.slice(); // 얕은 복사임, slice(시작인덱스, 개수? 끝나는인덱스+1) 인수없으면 전체복사.
-    console.log('nextSquares : ',nextSquares)
     let OX = (xIsNext) ? "X" : "O";
+
+    // if (nextSquares[i] != null) return;
+    if (nextSquares[i]) return; // 값이 존재하면
     nextSquares[i] = OX;
-    setXIsNext(!xIsNext);
+
     setSquares(nextSquares);
-    console.log(squares)
+    setXIsNext(!xIsNext);
+  
   }
 
+    const winner = calculateWinner(squares);
+    let status;
+    const nextPlayer = (xIsNext) ? "X" : "O";
+    status = (winner) ? "Winner: "+winner : "Next Player: "+nextPlayer;
 
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={()=> handleClick(0)} />
         <Square value={squares[1]} onSquareClick={()=> handleClick(1)} />
